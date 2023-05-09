37c4ecffc8c86158973d4722f378f2fda8539746
seungmio <68382824+seungmio@users.noreply.github.com>
2023-05-09 16:29:56 +0900

    chore: 프로젝트 기본 설정

---
+++ src/main/java/swu/musling/MuslingApplication.java
@@ -0,0 +1,13 @@
+package swu.musling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuslingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuslingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/swu/musling/MuslingApplicationTests.java
@@ -0,0 +1,13 @@
+package swu.musling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuslingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
